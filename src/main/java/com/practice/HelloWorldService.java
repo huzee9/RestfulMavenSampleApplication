166ee4d77cb5b426a900bd8adac2f109e1a2da94
huzee9 <33800624+huzee9@users.noreply.github.com>
2021-05-24 20:01:22 +0530

    first commit

---
+++ src/main/java/com/practice/HelloWorldService.java
@@ -0,0 +1,25 @@
+package com.practice;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+@Path("/customers")
+public class HelloWorldService {
+	
+	@GET
+	@Produces("text/html")
+	public Response getLocalCust() {
+		String output = "I am from getLocalCust Method";
+		return Response.status(200).entity(output).build();
+	}
+	
+	@GET
+	@Path("/nri")
+	@Produces("text/html")
+	public Response getNriCust() {
+		String output = "I am from getNriCust Method";
+		return Response.status(200).entity(output).build();
+	}
+}
